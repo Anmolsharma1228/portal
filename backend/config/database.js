17be92ada9599e86e4cb6baaf1ad91d3ab9c249f
Anmol sharma <as7067647@gmail.com>
2025-09-12 18:13:50 +0530

    add new database

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
+    usenewUrlParser: true;
+    useUnifiedTopology: true;
     console.log("MongoDB connected");
   } catch (err) {
     console.log(err);
